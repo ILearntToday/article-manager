ae4d4a3a4400ba359d852f727bde7ea960368c5b
DHIRAJ KUMAR <dhiraj.kumar2@microfocus.com>
2020-07-11 13:43:57 +0530

    reponse body VO added

--- src/main/java/com/iLearntToday/articleManagentService/entity/ResponseVO.java
+++ src/main/java/com/iLearntToday/articleManagentService/entity/ResponseVO.java
@@ -3,12 +3,12 @@ package com.iLearntToday.articleManagentService.entity;
 import lombok.Data;
 
 @Data
-public class ResponseStatus {
+public class ResponseVO {
 
     int responseCode;
     String message;
 
-    public ResponseStatus(int responseCode, String message) {
+    public ResponseVO(int responseCode, String message) {
         this.responseCode = responseCode;
         this.message = message;
     }
